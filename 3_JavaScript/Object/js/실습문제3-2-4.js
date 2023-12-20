88aac146e002b5eddf91d40ece643d7ba3784aff
jungdj221 <152463270+jungdj221@users.noreply.github.com>
2023-12-21 08:45:51 +0900

    regExp

--- 3_JavaScript/Object/js/실습문제3-2-4.js
+++ 3_JavaScript/Object/js/실습문제3-2-4.js
@@ -1,2 +1,23 @@
 //1. 입력시 글자 반짝
-const userId = document.querySelector(".userid");
+
+userId.addEventListener("input", function(){
+    const regExp = /^[a-zA-Z][a-zA-Z0-9]{3,11}$/;
+    if (regExp.test(userId.value)){
+      userIdResult.style.color = "green";
+      userIdResult.innerHTML ="good, move on next!";
+    } else {
+      userIdResult.style.color="red"
+      userIdResult.innerHTML ="글자는 반드시 영문자로, 그리고 영문자, 숫자 포함 총4~12자입력"
+    }
+});
+// userId.addEventListener("input", function () {
+//     const regExp = /^[a-zA-Z][a-zA-Z0-9]{3,11}$/;
+//     if (regExp.test(userId.value)) {
+//       userIdResult.style.color = "green";
+//       userIdResult.innerHTML = "OK!";
+//     } else {
+//       userIdResult.style.color = "red";
+//       userIdResult.innerHTML =
+//         "첫글자는 반드시 영문자로, 그리고 영문자, 숫자 포함하여 총 4~12자로 입력하시오.";
+//     }
+//   });
