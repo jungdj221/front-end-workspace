eec4d34f493b87ed71f177f7f0b2ac927d1f67ac
jungdj221 <152463270+jungdj221@users.noreply.github.com>
2023-12-20 12:50:05 +0900

    coupang02_2023-12-20/12:50pm

--- coupang/script.js
+++ coupang/script.js
@@ -12,27 +12,37 @@ navIcons[1].addEventListener("click", function () {
   });
 });
 
-// category
-const categoryBtn = document.querySelector(".category-btn");
-const category = document.querySelector(".category");
+// main
+const mainImg = document.querySelectorAll("main > img");
+//첫 번째 이미제 main-img-active클래스가 추가되어 있음
+setInterval(() => {
+  //const className = "main-img-active";
+  for (let i = 0; i < mainImg.length; i++) {
+    //마지막 이미지에서는 클래스 제거, 첫번째 이미지에 클래스추가
 
-categoryBtn.addEventListener("mouseenter", function () {
-  category.style.display = "flex";
-});
-category.addEventListener("mouseleave", function () {
-  category.style.display = "none";
-});
-
-const categoryItems = document.querySelectorAll(".category-item");
-const categorySub = document.querySelector(".category-sub");
-const categorySubItems = document.querySelectorAll(".category-sub-item");
-
-categoryItems.forEach((item, index) => {
-  item.addEventListener("mouseenter", function () {
-    categorySub.style.display = "block";
-    categorySubItems[index].style.display = "block";
-  });
-  item.addEventListener("mouseleave", function () {
-    categorySubItems[index].style.display = "none";
-  });
+    //첫번째 이미지에서는 클래스 제거, 두번쨰 이미지에 클래스 추가
+    //두번째 이미지에서는 클래스 제거, 세번째 이미지에 클래스 추가
+    if (mainImg[i].classList.contains("main-img-active")) {
+      mainImg[i].classList.remove("main-img-active");
+      //마지막꺼만 예외로 처리
+      if (i === mainImg.length - 1) {
+        mainImg[0].classList.add("main-img-active");
+        break;
+      }
+      mainImg[i + 1].classList.add("main-img-active");
+      break;
+    }
+  }
+}, 2000);
+// section: 카테고리별 추천광고상품
+//사용자가 스크롤을 내리는 순간 어느 시점에 나타나고
+//다시 올리면 사라지는
+const categoryMenu = document.querySelector(".category-menu");
+window.addEventListener("scroll", function () {
+  //console.log(window.scrollY);
+  if (window.scrollY >= 1300) {
+    categoryMenu.style.display = "flex";
+  } else {
+    categoryMenu.style.display = "none";
+  }
 });
