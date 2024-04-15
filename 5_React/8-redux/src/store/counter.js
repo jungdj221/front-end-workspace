9c1f35231dfd3d915c0a70b1c38450a82308565f
jungdj221 <152463270+jungdj221@users.noreply.github.com>
2024-04-15 12:42:19 +0900

    08-redux toolkit 사용

--- 5_React/8-redux/src/store/counter.js
+++ 5_React/8-redux/src/store/counter.js
@@ -1,21 +1,36 @@
-import { createAction, handleActions } from "redux-actions";
+// import { createAction, handleActions } from "redux-actions";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Action : 프로젝트의 상태에 변화를 일으키는 것
-const INCREASE = "counter/INCREASE";
-const DECREASE = "counter/DECREASE";
+// // Action : 프로젝트의 상태에 변화를 일으키는 것
+// const INCREASE = "counter/INCREASE";
+// const DECREASE = "counter/DECREASE";
 
-// 액션 생성 함수(action creator) : action 객체를 만들어 주는 함수
-export const increase = createAction(INCREASE);
-export const decrease = createAction(DECREASE);
+// // 액션 생성 함수(action creator) : action 객체를 만들어 주는 함수
+// export const increase = createAction(INCREASE);
+// export const decrease = createAction(DECREASE);
 
-// reducer : 변화를 일으키는 함수
-const counter = handleActions(
-  {
-    // 이게 초기값
-    [INCREASE]: (state, action) => state + 1,
-    [DECREASE]: (state, action) => state - 1,
+// // reducer : 변화를 일으키는 함수
+// const counter = handleActions(
+//   {
+//     // 이게 초기값
+//     [INCREASE]: (state, action) => state + 1,
+//     [DECREASE]: (state, action) => state - 1,
+//   },
+//   0 // 초기값
+// );
+
+const counter = createSlice({
+  name: "counterSlice",
+  initialState: 0,
+  reducers: {
+    increase: (state, action) => {
+      return state + 1;
+    },
+    decrease: (state, action) => {
+      return state - 1;
+    },
   },
-  0 // 초기값
-);
+});
 
 export default counter;
+export const { increase, decrease } = counter.actions;
--- 5_React/8-redux/src/store/index.js
+++ 5_React/8-redux/src/store/index.js
@@ -1,8 +1,16 @@
-import { combineReducers } from "redux";
+// import { combineReducers } from "redux";
 import counter from "./counter";
+import { configureStore } from "@reduxjs/toolkit"; // toolkit
 
-const store = combineReducers({
-  counter,
+// const store = combineReducers({
+//   counter,
+// });
+
+// toolkit version
+const store = configureStore({
+  reducer: {
+    counter: counter.reducer,
+  },
 });
 
 export default store;
